88dbc3be8be3a3129cfca8e4e60877447d4f73b1
Elijah Jian <hjxtty@outlook.com>
2019-12-08 00:37:38 +0800

    模块 PWDManage 的初始签入

---
+++ PWDManage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PWDManage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
